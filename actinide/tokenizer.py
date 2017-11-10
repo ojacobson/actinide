e4fb8604aa2fc572a3aeeace1c32de7339d346b5
Owen Jacobson <owen@grimoire.ca>
2017-11-10 01:25:30 -0500

    Testing fixes.
    * Add a top-level test that roundtrips sequences of tokens. (This found a real bug. Thanks, Hypothesis!)
    
    * Remove type conversion from the tokenizer. This simplifies the code, and makes testing considerably easier.
    
    * Fix some bugs in string literal parsing (again: Thanks, Hypothesis!)
    
    Document the test cases, and the case-by-case strategy, better.
    
    This also involved prying apart some tests that cover multiple cases.
    
    Stop treating empty strings as if they were EOFs. (Thanks, Hypothesis!)
    
    fixup! Stop treating empty strings as if they were EOFs. (Thanks, Hypothesis!)
    
    Remove type conversion from the tokenizer.
    
    It turns out that this made the tokenizer harder to test, because it was doing too many things. The tokenizer now _only_ divides the input port into tokens, without parsing or converting those tokens.
    
    Fix up tests for fuck

--- actinide/tokenizer.py
+++ actinide/tokenizer.py
@@ -1,5 +1,3 @@
-from .types import *
-
 # ## TOKENIZATION
 #
 # The following code implements a state-machine-driven tokenizer which can
@@ -8,19 +6,20 @@ from .types import *
 #
 # * Comments: ``;`` followed by all bytes to EOF or to the end of the line.
 #
-# * Strings: ``"`` through to the next unescaped ``"`` are read, de-escaped, and
-#   returned. The sequences ``\"`` and ``\\`` are treated specially: the former
-#   de-escapes to ``"``, and the latter to ``\``. An unclosed string literal or
-#   an unknown escape sequence is a tokenization error.
-#
 # * Open and close parens: ``(`` and ``)`` are returned as freestanding tokens.
 #
 # * Whitespace: Space, horizontal tab, and newline characters are discarded
 #   during tokenization.
 #
-# * Symbols: Any sequence of characters not included in one of the above classes
+# * Strings: ``"`` through to the next unescaped ``"`` are read and returned.
+#   Sequences within the string beginning with ``\`` indicate an escape, and may
+#   only be followed by the character ``"`` or ``\``. An unclosed string literal
+#   or an unknown escape sequence is a tokenization error.
+#
+# * Atoms: Any sequence of characters not included in one of the above classes
 #   is read and returned as a single token. This includes words, numbers, and
-#   special literals.
+#   special literals. (Strings are, technically, a kind of atom, but the lexer
+#   treats them specially due to their complexity.)
 #
 # Internally, the tokenizer is a state machine which maintains two pieces of
 # state: the "lookahead" (holding data to feed to the next state transition
@@ -34,6 +33,11 @@ from .types import *
 # port) to determine the next state, and return a 3-tuple of ``token`` (may be
 # ``None``), ``lookahead`` (which replaces the previous lookahead), and ``next``
 # (the new next state transition function).
+#
+# This is heavily inspired by various tail-recursive approaches to tokenizing
+# lisp streams. However, the host language does not guarantee tail call
+# optimizations, so we use an explicit trampoline function to drive the state
+# machine instead of calling each parser directly.
 
 class TokenError(Exception):
     '''
@@ -47,20 +51,20 @@ class TokenError(Exception):
 #
 # This is the top-level driver for the state machine that divides the underlying
 # input into tokens. It does no input handling itself, other than reading the
-# first character of the port: so long as the lookahead is non-empty, this calls
-# the next state transition function to determine what to do and how to change
-# the lookahead.
+# first character of the port: this calls the next state transition function to
+# determine what to do and how to change the lookahead.
 #
-# Initially, this is in the ``tokenize_any`` state.
+# Initially, this is in the ``tokenize_any`` state, and exits once it reaches
+# the ``tokenize_eof`` state.
 def tokenize(port):
     lookahead, next = port.read(1), tokenize_any
-    while len(lookahead) > 0:
+    while next != tokenize_eof:
         token, lookahead, next = next(lookahead, port)
         if token is not None:
             yield token
 
-# If the lookahead is exactly one read result, this will correctly determine the
-# next token type and return that state without consuming input. This is
+# If the lookahead is exactly one character, this will correctly determine the
+# next token type and transition to that state without consuming input. This is
 # generally the correct state to transition to any time the next token is
 # unknown - for example, at the end of another token.
 #
@@ -75,9 +79,7 @@ def tokenize_any(lookahead, port):
         return None, lookahead, tokenize_syntax
     if lookahead in ' \t\n':
         return None, lookahead, tokenize_whitespace
-    if lookahead == '"':
-        return None, lookahead, tokenize_string
-    return None, lookahead, tokenize_symbol
+    return None, lookahead, tokenize_atom
 
 # Special trap state. This never produces a token, and always transitions to
 # itself. The lookahead in this state is generally ``''``, and since this never
@@ -89,8 +91,8 @@ def tokenize_any(lookahead, port):
 def tokenize_eof(lookahead, port):
     return None, lookahead, tokenize_eof
 
-# Consumes one read result at a time until it finds an end of line or runs out
-# of input. This throws away comments entirely, at parse time, without
+# Consumes one character at a time until it finds an end of line or runs out of
+# input. This throws away comments entirely, at tokenization time, without
 # considering whether the comment content can be separated into tokens. As this
 # scans the comment, the lookahead will be set to successive characters from the
 # port, but never more than one character at a time.
@@ -104,51 +106,64 @@ def tokenize_comment(lookahead, port):
         return None, next, tokenize_any
     return None, next, tokenize_comment
 
-# Consumes the lookahead and packages it up as a Syntax token. This is generally
-# appropriate for the ``(`` and ``)`` syntactic elements.
+# Generates the entire lookahead as a token. This is generally appropriate for
+# the ``(`` and ``)`` syntactic elements.
 #
 # The resulting lookahead will be the next character of input, and this always
 # dispatches back to ``tokenize_any`` so that the next token (if any) can be
 # determined.
 def tokenize_syntax(lookahead, port):
-    return Syntax(lookahead), port.read(1), tokenize_any
+    return lookahead, port.read(1), tokenize_any
 
-# Consumes and ignores whitespace in the input. This never produces a token, and
+# Consumes and ignores one character of input. This never produces a token, and
 # throws away the lookahead entirely. The resulting lookahead is the next
 # character of input.
 def tokenize_whitespace(lookahead, port):
     return None, port.read(1), tokenize_any
 
-# Consumes characters until it finds a character which cannot be part of a token
-# or until it finds the end of input, accumulating them into a single Symbol
-# token. This is a heavily-overloaded token category, as it contains not only
-# Actinide symbols but also all non-String literals.
+# We've ruled out all non-atom tokens. If the lookahead is a string delimiter,
+# transitions to a state which tokenizes a single string literal; otherwise,
+# transitions to a state which consumes a single non-string atom. In both cases,
+# this leaves the lookahead alone, and generates no token.
+def tokenize_atom(lookahead, port):
+    if lookahead == '"':
+        return None, lookahead, tokenize_string
+    return None, lookahead, tokenize_nonstring_atom
+
+# Consumes characters until it finds a character which cannot be part of a
+# non-string atom, or until it finds the end of input, accumulating them into a
+# single token. This is a heavily-overloaded token category, as it contains not
+# only Actinide symbols but also all non-String literals.
 #
 # While the tokenizer remains in this state, the lookahead accumulates the
 # characters of the token. When this matches a completed token, it produces a
 # Symbol token, and resets the lookahead back to a single read result containing
 # the next character of input.
-def tokenize_symbol(lookahead, port):
+def tokenize_nonstring_atom(lookahead, port):
     next = port.read(1)
     if next == '':
-        return Symbol(lookahead), next, tokenize_any
+        return lookahead, next, tokenize_any
     if next in '"(); \t\n':
-        return Symbol(lookahead), next, tokenize_any
-    return None, lookahead + next, tokenize_symbol
+        return lookahead, next, tokenize_any
+    return None, lookahead + next, tokenize_nonstring_atom
 
 # ### STRINGS
 #
-# The following states handle string literals in the input stream. String
-# literals are fairly simple: they begin with a quote, contain arbitrary
-# characters other than a bare \ or ", and end with a quote. The sequences
-# ``\\`` and ``\"`` are de-escaped by removing the leading backslash and
-# included in the resulting string.
+# The following family of states handles string literals in the input stream.
+# String literals are fairly simple: they begin with a quote, contain arbitrary
+# characters other than a bare \ or ", and end with a quote. (Note that ``\n``
+# is not an escape sequence: unescaped newlines are permitted within string
+# literals.)
 #
 # These states use the lookahead to accumulate the characters of the string. On
 # transition back to ``tokenize_any``, the lookahead is always set back to a
 # single character. If, at any point, these states encounter EOF, they raise a
 # ``TokenError``: no legal token in Actinide begins with a quote mark and ends
 # with EOF.
+#
+# Because tokenization is only concerned with dividing the input into tokens,
+# this machine *does not* strip quotes or replace escape sequences. On success,
+# it generates a token containing the whole the string literal, verbatim.
 
 # The lookahead is assumed to be the opening quote of a string, and discarded.
 # Read forwards one character to determine whether this is an empty string
@@ -161,9 +176,11 @@ def tokenize_string(lookahead, port):
     next = port.read(1)
     if next == '':
         raise TokenError('Unclosed string literal')
+    if next == '\\':
+        return None, lookahead + next, tokenize_escaped_string_character
     if next == '"':
-        return None, '', tokenize_string_end
-    return None, next, tokenize_string_character
+        return None, lookahead + next, tokenize_string_end
+    return None, lookahead + next, tokenize_string_character
 
 # The lookahead contains the body of the string read so far. Reads forwards one
 # character to determine if the string continues, contains an escaped character,
@@ -175,15 +192,14 @@ def tokenize_string_character(lookahead, port):
     if next == '':
         raise TokenError('Unclosed string literal')
     if next == '\\':
-        return None, lookahead, tokenize_escaped_string_character
+        return None, lookahead + next, tokenize_escaped_string_character
     if next == '"':
-        return None, lookahead, tokenize_string_end
+        return None, lookahead + next, tokenize_string_end
     return None, lookahead + next, tokenize_string_character
 
 # The lookahead contains the body of the string so far. Reads forwards one
 # character to determine which, if any, escaped character to process: if it's
-# one we recognize, de-escape it and append it to the string, otherwise raise a
-# TokenError.
+# one we recognize, append it to the string, otherwise raise a TokenError.
 #
 # This never yields a token, and always dispatches back to
 # ``tokenize_string_character`` on a legal escape character.
@@ -191,14 +207,14 @@ def tokenize_escaped_string_character(lookahead, port):
     next = port.read(1)
     if next == '':
         raise TokenError('Unclosed string literal')
-    if next == 'n':
-        return None, lookahead + '\n', tokenize_string_character
+    if next == '"':
+        return None, lookahead + next, tokenize_string_character
     if next == '\\':
-        return None, lookahead + '\\', tokenize_string_character
-    raise TokenError(f"Unknown string escape '\{next}'")
+        return None, lookahead + next, tokenize_string_character
+    raise TokenError(f"Invalid string escape '\\{next}'")
 
 # Package the lookahead (the full string body, de-escaped and without leading
 # and trailing quotes) up as a String token and return it, then transition back
 # to the ``tokenize_any`` state with a single read result in the lookahead.
 def tokenize_string_end(lookahead, port):
-    return String(lookahead), port.read(1), tokenize_any
+    return lookahead, port.read(1), tokenize_any
---
+++ tests/tokens.py
@@ -0,0 +1,90 @@
+from hypothesis.strategies import just, one_of, characters, text, lists, tuples
+from hypothesis.strategies import composite, recursive
+
+# Generators for token families
+
+# Generates the `(` token.
+def open_parens():
+    return just('(')
+
+# Generates the ')' token.
+def close_parens():
+    return just(')')
+
+# Generates characters that are legal, unescaped, inside of a string.
+def string_bare_characters():
+    return characters(blacklist_characters='\\"')
+
+# Generates legal string escape sequences.
+def string_escaped_characters():
+    return one_of(just('"'), just('\\')).map(lambda c: '\\' + c)
+
+# Generates single-character string representations, including escapes.
+def string_characters():
+    return one_of(string_bare_characters(), string_escaped_characters())
+
+# Generates arbitrary string bodies (strings, without leading or trailing
+# quotes)
+def string_body():
+    return text(string_characters())
+
+# Generates legal strings.
+def strings():
+    return tuples(just('"'), string_body(), just('"')).map(lambda t: ''.join(t))
+
+# Generates characters which are legal within a symbol.
+def symbol_characters():
+    return characters(blacklist_characters=' \t\n();"')
+
+# Generates legal symbols.
+def symbols():
+    return text(symbol_characters(), min_size=1)
+
+# Generates single whitespace characters.
+def whitespace_characters():
+    return one_of(just('\n'), just(' '), just('\t'))
+
+# Generates a single token.
+def tokens():
+    return one_of(symbols(), strings(), open_parens(), close_parens())
+
+# Generates at least one character of whitespace.
+def whitespace():
+    return text(whitespace_characters(), min_size=1)
+
+# Generates characters which can legally appear inside of a comment (anything
+# but a newline).
+def comment_characters():
+    return characters(blacklist_characters='\n')
+
+# Generates a (possibly-empty) comment, terminated with a trailing newline.
+def comments():
+    return tuples(just(';'), text(comment_characters()), just('\n')).map(lambda t: ''.join(t))
+
+# Generates sequences which can be inserted between arbitrary pairs of tokens
+# without changing their meaning.
+def intertokens():
+    return one_of(comments(), whitespace())
+
+# Generate a pair such that the second element is a token, and joining the
+# elements with an empty string produces a string that tokenizes to the second
+# element.
+def spaced_tokens():
+    def spaced(strategy):
+        return tuples(intertokens(), strategy)
+    def unspaced(strategy):
+        return tuples(one_of(just(''), intertokens()), strategy)
+    def spaced_symbols():
+        return spaced(symbols())
+    def spaced_strings():
+        return unspaced(strings())
+    def spaced_open_parens():
+        return unspaced(open_parens())
+    def spaced_close_parens():
+        return unspaced(close_parens())
+
+    return one_of(spaced_symbols(), spaced_strings(), spaced_open_parens(), spaced_close_parens())
+
+# Generats a list of pairs as per spaced_token().
+def spaced_token_sequences():
+    return lists(spaced_tokens())
