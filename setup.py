64b913d16c8bfe93ceca7b37316909ad1d6f4d26
Owen Jacobson <owen@grimoire.ca>
2017-11-18 22:20:51 -0500

    Stop installing tests by default.

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='actinide',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(
+        exclude=['tests', 'tests.*'],
+    ),
     scripts=['bin/actinide-repl'],
 
     setup_requires=[
